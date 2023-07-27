6a47c74d184516e79e4baf951bbef3c356d2b8fa
luba73/luba73 <tkachik121273@gmail.com>
2023-07-27 19:24:51 +0300

    icon

--- src/resources/main.js
+++ src/resources/main.js
@@ -3,17 +3,17 @@ var map;
 
 DG.then(function () {
     map = DG.map('map', {
-        center: [51.545863, 46.018537],
+        center: [51.532351, 46.020435],
         zoom: 18
     });
-    // DG.marker([51.545863, 46.018537]).addTo(map).bindPopup('Режим работы: 08:00 - 20:00');
+    // DG.marker([51.532351, 46.020435]).addTo(map).bindPopup('Режим работы: 08:00 - 20:00');
     var myIcon = DG.icon({
-        iconUrl: './images/icon.png',
+        iconUrl: '../images/iconBell.png',
         iconSize: [30, 30],
-        iconAnchor: [35, 35],
+        iconAnchor: [0, 35],
         popupAnchor: [-20, -20],
         shadowAnchor: [22, 94]
     });
     
-    DG.marker([51.545863, 46.018537], {icon: myIcon}).addTo(map).bindPopup('Режим&nbsp;работы: <br> 08:00 - 20:00');
+    DG.marker([51.532351, 46.020435], {icon: myIcon}).addTo(map).bindPopup('Режим&nbsp;работы: <br> 08:00 - 20:00');
 });
